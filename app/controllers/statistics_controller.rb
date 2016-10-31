da50810aad03d41661767d082caed8fe240adf8a
SluvsP <salomekbg@gmail.com>
2016-10-31 12:43:59 -0400

    Add more styling, analytics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,13 +2,23 @@ class StatisticsController < ApplicationController
   def index
     find_user_by_session_id
     @rooms = Room.all
-    sql = <<-SQL
-      SELECT name
+    columns = <<-SQL
+      SELECT topics.*
       FROM topics
-      JOIN rooms
-      ON topic.id = rooms.topic_id
-      GROUP BY topics.id
+      -- JOIN rooms
+      -- ON topics.id = rooms.topic_id
+      -- GROUP BY topics.id
     SQL
-    puts "hi"
+
+    topic_array = Topic.find_by_sql(columns)
+    @topic_names = topic_array.map {|topic_object| [topic_object[:name], topic_object.rooms.length]}
+
+    bar = <<-SQL
+      SELECT rooms.*
+      FROM rooms
+    SQL
+
+    room_array = Room.find_by_sql(bar)
+    @room_users = room_array.map {|room_object| [room_object[:name], room_object.users.length]}
   end
 end
