255409e2a5ba395847a18ee66d5f7b35b508ff5c
github username <github email address>
2016-10-31 18:16:46 -0400

    add services, timestamps, post limit.. etc

--- app/models/room.rb
+++ app/models/room.rb
@@ -10,14 +10,40 @@
 #
 
 class Room < ApplicationRecord
-  # validates :name, presence: true, uniqueness: {scope: :topic}, on: :create
+  validates :name, presence: true, uniqueness: {scope: :topic}, on: :create
 
   belongs_to :topic
   has_many :memberships
   has_many :posts, through: :memberships
   has_many :users, through: :memberships
 
+  def most_narcissistic
+    Room.all.map {|room| room.narcissism}.sort.last
+  end
+
+  def narcissism
+    allposts = self.posts.map{|post| post.content }.join(" ").upcase
+    allposts.split(" ").count("I")
+  end
+
   def users_count
+    byebug
     self.users.uniq.count
+  end
+
+  def posts_count
+    self.posts.count
+  end
+
+  def last_post
+    self.posts.last
+  end
+
+  def current_members
+    self.users.uniq
+  end
+
+  def total_posts
+    self.posts.count
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Topick
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # config.autoload_paths= Dir["#{config.root}/app/services"]
     config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
