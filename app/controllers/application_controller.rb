a670a6df7465540a8be38fbd33aa9a05849e78ce
SluvsP <salomekbg@gmail.com>
2017-03-16 15:32:09 -0400

    remove unused code from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-    # helper_method :deleted_user
-
-  # def deleted_user
-  #   @user = User.find_by(username: "deleted_user")
-  # end
-
   def current_user
     if session[:user_id]
       user = User.find(session[:user_id])
